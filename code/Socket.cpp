cd87eab45ab422ab12e12daaa0ed089bad2c5858
Cookie-27 <3220822474@qq.com>
2024-01-11 23:23:25 +0800

    more

--- code/Socket.cpp
+++ code/Socket.cpp
@@ -4,6 +4,8 @@
 #include <cstring>
 #include <arpa/inet.h>
 #include "common.h"
+#include <sys/ioctl.h>
+#include <fcntl.h>
 
 Socket::Socket():fd_(-1)
 {
@@ -78,30 +80,87 @@ RS Socket::Bind(const char *ip, uint16_t port) const
 
 RS Socket::Listen() const
 {
+    if (listen(fd_, SOMAXCONN) == -1) {
+        //throw std::runtime_error("Failed to listen on the socket");
+    }
 
+    // 设置监听状态成功，返回成功状态或其他适当的值
+    return RS::RS_SUCCES;
 }
 
 RS Socket::Accept(int &clnt_fd) const
 {
-    
+
+    clnt_fd = accept(fd_, NULL, NULL);
+
+    if (clnt_fd == -1) {
+        // 接受连接失败，可以根据实际情况处理错误
+        //throw std::runtime_error("Failed to accept client connection");
+        return RS::RS_DEFEAT;
+    }
+
+    // 接受连接成功，返回成功状态或其他适当的值
+    return RS::RS_SUCCES;
 }
 
 RS Socket::Connect(const char *ip, uint16_t port) const
 {
-
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = inet_addr(ip);
+    addr.sin_port = htons(port);
+    if (::connect(fd_, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
+        perror("Failed to connect socket");
+        return RS::RS_DEFEAT;
+    }
+    return RS::RS_SUCCES;
 }
 
-RS Socket::SetBlock() const
-{
+RS Socket::SetNonBlock() const {
+  return SetBlockInternal(true);
+}
+RS Socket::SetBlock() const{
+  return SetBlockInternal(false);
+}
 
+RS Socket::SetBlockInternal(bool non_block) const {
+  int flags;
+  if ((flags = fcntl(fd_, F_GETFL)) == -1) {
+    //perror("Socket fcntl(F_GETFL) error");
+    return RS::RS_DEFEAT;
+  }
+  if (!!(flags & O_NONBLOCK) == !!non_block)
+    return RS::RS_SUCCES;
+  if (non_block) 
+    flags |= O_NONBLOCK;
+  else
+    flags &= ~O_NONBLOCK;
+  
+  if (fcntl(fd_, F_SETFL, flags) == -1) {
+    //perror("Socket fcntl(F_SETFL) error");
+    //return RC_SOCKET_ERROR;
+  }
+
+  return RS::RS_SUCCES;
 }
 
 bool Socket::IsNonBlock() const
 {
-
+    return (fcntl(fd_, F_GETFL) & O_NONBLOCK) != 0;
 }
 
 size_t Socket::RecvBufSize() const
 {
+    int size;
+    socklen_t len = sizeof(size);
 
+    // 查询套接字接收缓冲区大小
+    if (getsockopt(fd_, SOL_SOCKET, SO_RCVBUF, &size, &len) == -1) {
+        // 查询失败，可以根据实际情况处理错误
+        //throw std::runtime_error("Failed to get receive buffer size");
+    }
+
+    // 返回接收缓冲区大小
+    return static_cast<size_t>(size);
 }
